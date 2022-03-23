0770c87702d968016cbe1ec4bf8ee4662fd03db8
Oussama Bessaad <93039035+oussama24bessaad@users.noreply.github.com>
2022-03-23 12:54:20 +0100

    Code Smells removed

--- BackendApp/server.js
+++ BackendApp/server.js
@@ -6,23 +6,16 @@ app.use(cors())
 require('dotenv').config()
 app.use(express.json())
 
-// const connectDB = require('./helpers/connectDB');
-//   connectDB();
-// mongoose.connect(process.env.MONGO_URI,(err)=>(
-//  err ? console.log(err) : console.log('database connected')
-// )) 
 
-// //mongoose setup
 const connectDB = require('./helpers/connectDB');
 connectDB();
 
 const User = require('./models/userModel')
-// const { connect } = require('./routes/userRoutes')
 
 
-// routes
+
+
 app.use('/api/users',require('./routes/userRoutes'))
-// User.create({name:'oussama',email:'oussama24bessaad@gmail.com'})
 
 const port = process.env.PORT || 5000;
 app.listen(port, (err) => {
